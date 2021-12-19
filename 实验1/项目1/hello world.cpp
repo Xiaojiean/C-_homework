3fe874e398205db63667b5ec96a70849d4c792dc
zhangzhanteng <91193560+zhangzhanteng@users.noreply.github.com>
2021-12-19 14:31:27 +0800

    Add files via upload

---
+++ 实验1/项目1/hello world.cpp
@@ -0,0 +1,16 @@
+//版权声明
+//Copyright 2021-2099 ZhantengZhang.
+//202030310284 张展腾 
+
+#include  <iostream>
+using namespace std;
+int main()
+{
+	int age;
+	cout << "please input your age:" << endl;
+	cin >> age;
+	cout << "hello world!I am " << age << endl;
+	int i=500000000000; 
+	for(;i>=1;i--);
+	return 0;
+}
---
+++ 实验4/项目4/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,44 @@
+﻿//版权声明
+//Copyright 2021-2099 ZhantengZhang.
+//202030310284 张展腾
+
+#include <iostream>
+#include <string>
+using namespace std;
+static float classmoney;
+class tstudent {
+private:
+	string name[100];
+public:
+	
+	tstudent()
+	{
+		classmoney = 1000;//解析函数初始化
+	}
+	/*void initstudent(string name1)
+	{
+		int i = 1;
+		name[i] = name1;
+		++i;
+	}*/
+	void expendmoney(float money)
+	{
+		classmoney = classmoney - money;
+	}
+	void showmoney()
+	{
+		cout << "班级的余额为"<<classmoney << endl;
+	}
+};
+int main()
+{
+	tstudent A, B, C;
+
+	A.expendmoney(50);
+	A.showmoney();
+	B.expendmoney(98.5);
+	B.showmoney();
+	C.expendmoney(500.53);
+	C.showmoney();
+	
+}
