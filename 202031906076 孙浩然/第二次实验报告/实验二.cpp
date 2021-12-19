23a14105fbff9ae1760ecaec682100a236c0de99
N1ght-master <91146639+N1ght-master@users.noreply.github.com>
2021-12-20 00:55:21 +0800

    Add files via upload

---
+++ 202031906076 孙浩然/第二次实验报告/实验二.cpp
@@ -0,0 +1,15 @@
+﻿int Min(int* arr)
+{
+    int mi = arr[0];
+
+    for (int i = 0; i < 5;i++)
+    {
+        if (mi > arr[i])
+        {
+            mi = arr[i];
+        }
+    }
+
+    return mi;
+}
+//Copyright ©2021-2099 HaoranSun. All rights reserved
